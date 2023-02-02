30838b64d02de5228b039474c12fafb4a0a580df
Odame <kwesi.danquah@daaseng.com>
2023-02-02 01:35:48 +0000

    Setup tailwind css

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,12 +6,26 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .react()
+    .sass('resources/sass/app.scss', 'public/css');
+
+mix.js('resources/js/app.js', 'public/js')
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css')
     .postCss('resources/css/app.css', 'public/css', [
-        //
+        require('tailwindcss')
     ]);
+    // .browserSync(
+    //     {
+    //         'host' : 'localhost',
+    //         'proxy' : 'fovea_app',
+    //         'port' : 3000,
+    //         'open': false
+    //     }
+    // );
