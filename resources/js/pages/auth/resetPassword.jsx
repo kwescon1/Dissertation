e35abef7c0b7178344009c1def7e9f2e294fb516
Robert_Rolky <robert.yeboah@daaseng.com>
2023-02-20 09:19:56 +0000

    create auth pages

---
+++ resources/js/pages/auth/resetPassword.jsx
@@ -0,0 +1,25 @@
+import { Form } from "react-router-dom";
+import AuthBackground from "../../components/authBackground";
+import AuthButton from "../../components/authbutton";
+import AuthInput from "../../components/authInput";
+
+const ResetPassword = () => {
+    return ( 
+        <div className=" ">
+            <AuthBackground>
+                <div className="space-y-6">
+                    <span className="font-medium ">Set your new password</span>
+                    <Form className="space-y-4">
+                        <AuthInput type="password" placeholder="Password"  name="password"  />
+                        <AuthInput type="password" placeholder="Confirm Password"  name="confirmPassword"  />
+                        <AuthButton btnText="Save Password" linkText="Back to Login" linkTo="/login"  />
+                    </Form>
+                    
+                </div>
+               
+            </AuthBackground>
+        </div>
+     );
+}
+ 
+export default ResetPassword;
