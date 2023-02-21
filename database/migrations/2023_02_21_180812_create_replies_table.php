e5812ec74bae888d361c905a2e2cec9aa6517755
Odame <kwesi.danquah@daaseng.com>
2023-02-21 18:34:47 +0000

    configure chatbot db

---
+++ database/migrations/2023_02_21_180812_create_replies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('question_id')->constrained('questions');
+            $table->string('from');
+            $table->text('question');
+            $table->string('answer')->nullable();
+            $table->string('next_id')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replies');
+    }
+}
