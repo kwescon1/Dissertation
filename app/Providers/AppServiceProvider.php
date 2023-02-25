9a8784ad0ff13c71ec006441b662f48d642784a5
Odame <kwesi.danquah@daaseng.com>
2023-02-25 18:03:34 +0000

    configure API responses

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +28,38 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Carbon::setWeekStartsAt(Carbon::SUNDAY);
+        Carbon::setWeekEndsAt(Carbon::SATURDAY);
+        //
+        Schema::defaultStringLength(191);
+        JsonResource::withoutwrapping();
+
+        Response::macro('success', function ($data, $customCode = NULL) {
+            return response()->json([
+                'data' => $data ?: null,
+                // 'customCode' => $customCode,
+            ]);
+        });
+
+        Response::macro('created', function ($data, $customCode = NULL) {
+            return response()->json([
+                'data' => $data ?: null,
+                // 'customCode' => $customCode
+            ], \Illuminate\Http\Response::HTTP_CREATED);
+        });
+
+        Response::macro('notfound', function ($error, $customCode = NULL) {
+            return response()->json([
+                'error' => $error,
+                // 'customCode' => $customCode
+            ], \Illuminate\Http\Response::HTTP_NOT_FOUND);
+        });
+
+        Response::macro('error', function ($error, $statusCode = \Illuminate\Http\Response::HTTP_INTERNAL_SERVER_ERROR, $customCode = 9999) {
+            return response()->json([
+                'error' => $error,
+                // 'customCode' => $customCode,
+            ], $statusCode);
+        });
     }
 }
