ddcc7ae5961fadae7e7ecd0adef261e1938529bb
Odame <kwesi.danquah@daaseng.com>
2023-02-28 17:13:47 +0000

    chatbot migrations

---
+++ database/migrations/2023_02_28_121726_add_about_us_to_questions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAboutUsToQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            //
+        });
+
+        DB::table('questions')->insert([
+            "question" => "*Our AI chatbot is HIPAA-compliant, meaning all your medical data is secure and protected.*\n\n```Experience the convenience of healthcare like never before. Try our AI chatbot today and see the difference for yourself!```",
+            "options" => "*1.Back* \n*2.Quit*",
+            "method" => "ON_COMPLIANCE_MESSAGE_PROVIDED"
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            //
+        });
+    }
+}
