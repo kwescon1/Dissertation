a21d70b4b8b555f3e4d5f34136f618edffa23517
Odame <kwesi.danquah@daaseng.com>
2023-02-28 17:50:17 +0000

    configuring facility, facility branches and user apis

---
+++ database/migrations/2023_02_23_232405_create_facilities_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFacilitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('facilities', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->char('code', 3);
+            $table->tinyInteger('status');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('facilities');
+    }
+}
