8b96e27e5dc412548a67839782da946555105e68
Odame <kwesi.danquah@daaseng.com>
2023-02-28 17:15:01 +0000

    setup core bot configuartions

--- app/Services/Chatbot/Constants.php
+++ app/Services/Chatbot/Constants.php
@@ -4,7 +4,16 @@ namespace App\Services\Chatbot;
 
 class Constants
 {
+    //registration
     const INIT = 1;
+    const ABOUT_US = 2;
+    const REGISTER = 3;
+
+
+
+
+
+    // const INIT = 1;
 
 
     const CHOOSE_FROM_AVAILABLE_OPTIONS = 800;
