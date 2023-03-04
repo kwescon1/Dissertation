08e572d97d7cf0f03817277040ce3d947e612eec
Odame <kwesi.danquah@daaseng.com>
2023-03-04 15:54:48 +0000

    configure services

--- app/Services/Api/User/UserService.php
+++ app/Services/Api/User/UserService.php
@@ -2,8 +2,18 @@
 
 namespace App\Services\Api\User;
 
+use App\Models\User;
 use App\Services\Api\CoreService;
 
 class UserService extends CoreService implements UserServiceInterface
 {
+
+    /**
+     * @param string $username
+     * @return object?|NULL
+     */
+    public function findUserByUsername(string $username): ?object
+    {
+        return User::whereUsername($username)->first();
+    }
 }
