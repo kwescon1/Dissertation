2487da01e6078296eef49a250003c3d5f61ee1f8
Odame <kwesi.danquah@daaseng.com>
2023-03-04 15:45:08 +0000

    seed data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\Facility;
+use App\Models\FacilityBranch;
 use Illuminate\Database\Seeder;
+use App\Models\UserFacilityBranch;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +18,75 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        // create facility
+        echo "Creating facility \n";
+        $facility = Facility::factory()->create();
+
+        //seed default user
+        echo "creating default user instance \n";
+        $defaultUser = $this->defaultUser([
+            'facility_id' => $facility->id,
+            'firstname' => 'Soonleh',
+            'lastname' => 'Ling',
+            'email' => 'soonleh.ling@example.org',
+            'username' => 'soonleh.ling',
+            'password' => \Illuminate\Support\Facades\Hash::make('soonleh.ling')
+        ]);
+
+
+        echo "Creating facility branches\n";
+        FacilityBranch::factory()->count(3)
+            ->state([
+                'facility_id' => $facility->id
+            ])
+            ->create()
+            ->each(function ($facilityBranch) use ($facility, $defaultUser) {
+
+                //set head branch
+                if ($this->setBranchAsHeadBranch($facility->id)) {
+                    $facilityBranch->update(['is_head_branch' => true]);
+                }
+
+                //seed facility branch for default user
+                echo "creating default user facility branch \n";
+                $defaultUserFacilityBranch = $this->userFacilityBranch([
+                    'user_id' => $defaultUser->id,
+                    'facility_id' => $facility->id,
+                    'facility_branch_id' => $facilityBranch->id
+                ]);
+
+
+                //seed two users
+                echo "creating other users \n";
+                User::factory()->count(2)
+                    ->state([
+                        'facility_id' => $facility->id
+                    ])->create()
+                    ->each(function ($user) use ($facility, $facilityBranch) {
+                        echo "creating user facility branch \n";
+
+                        $userFacilityBranch = $this->userFacilityBranch([
+                            'user_id' => $user->id,
+                            'facility_id' => $facility->id,
+                            'facility_branch_id' => $facilityBranch->id
+                        ]);
+                    });
+            });
+    }
+
+    private static function defaultUser($userData = [])
+    {
+        return  User::factory()->state($userData)->create();
+    }
+
+    private static function userFacilityBranch($data = [])
+    {
+        return UserFacilityBranch::factory()->state($data)->create();
+    }
+
+    private static function setBranchAsHeadBranch($facilityId): bool
+    {
+        return !FacilityBranch::where("facility_id", $facilityId)->where(['is_head_branch' => true])->exists();
     }
 }
