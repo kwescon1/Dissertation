0d8e66f62332bcc03f10a584d90d0611ddff8cf1
Odame <kwesi.danquah@daaseng.com>
2023-03-15 23:26:12 +0000

    configure invalid link exception

---
+++ app/Exceptions/InvalidLinkException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Exceptions;
+
+use Laravel\SerializableClosure\Exceptions\InvalidSignatureException;
+
+class InvalidLinkException extends InvalidSignatureException
+{
+    //
+}
