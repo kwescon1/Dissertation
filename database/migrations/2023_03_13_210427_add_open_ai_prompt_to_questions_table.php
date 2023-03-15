a79561a71d479b05907b7e764ee64d1bd5438bc1
Odame <kwesi.danquah@daaseng.com>
2023-03-15 01:41:30 +0000

    configure open ai

---
+++ database/migrations/2023_03_13_210427_add_open_ai_prompt_to_questions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Question;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddOpenAiPromptToQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            //
+        });
+
+        DB::table('questions')->insert([
+            "question" => "Hi there, I am Clinton, your personal AI assistant. I answer any question asked relating to health issues.\n\nYour session with me would be terminated after 3️⃣0️⃣ minutes of inactivity⏳.\n\nShould you want to end this chat session, type *end*. How may I help you❓",
+            "options" => " ",
+            "method" => "onAskQuestionProvided"
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Question::whereMethod('onAskQuestionProvided')->delete();
+    }
+}
