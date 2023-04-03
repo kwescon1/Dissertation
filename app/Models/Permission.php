458354cf4a56b1c534c3e0caaf9ac6a53877cf41
Odame <kwesi.danquah@daaseng.com>
2023-04-03 01:24:47 +0100

    user roles and permissions

---
+++ app/Models/Permission.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use App\Utils\GeneratesUiud;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Permission\Models\Permission as SpatiePermission;
+
+class Permission extends SpatiePermission
+{
+    use HasFactory, GeneratesUiud;
+
+    protected $tableName = 'permissions';
+}
---
+++ app/Models/Role.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use App\Utils\GeneratesUiud;
+use Spatie\Permission\Guard;
+use Spatie\Permission\Models\Role as SpatieRole;
+use Spatie\Permission\Exceptions\RoleAlreadyExists;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Role extends SpatieRole
+{
+    use HasFactory, GeneratesUiud;
+
+    protected $tableName = 'roles';
+
+
+
+    /**
+     * override default create method from spatie.
+     * add facility_branch_id to $params array
+     * original create method does not have the facility_branch_id hence RoleAreadyExists error is thrown
+     * */
+    public static function create(array $attributes = [])
+    {
+        $attributes['guard_name'] = $attributes['guard_name'] ?? Guard::getDefaultName(static::class);
+
+        $params = [
+            'name' => $attributes['name'], 'guard_name' => $attributes['guard_name'],
+            'facility_branch_id' => $attributes['facility_branch_id']
+        ];
+
+        if (static::findByParam($params)) {
+            throw RoleAlreadyExists::create($attributes['name'], $attributes['guard_name']);
+        }
+
+        return static::query()->create($attributes);
+    }
+}
