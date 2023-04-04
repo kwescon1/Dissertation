479c5a1f31be7db70c26360f29314c5625ea4037
Odame <kwesi.danquah@daaseng.com>
2023-04-04 18:31:34 +0100

    feat(redirect to login page if user is unathenticated)

--- resources/js/layouts/index.jsx
+++ resources/js/layouts/index.jsx
@@ -1,38 +1,48 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { useEffect } from 'react';
+import { useState,useEffect } from 'react';
 import NavBar from "../components/navbar";
 import Sidebar from "../components/sidebar";
 import { setHeader } from "../services/token";
+import { getAuthUser } from "../services/storage";
+import { useNavigate ,Outlet} from "react-router-dom";
 
-
-
-const Index = () => {
+function Index(){
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null);
 
     useEffect(() => {
-        //set default headers if user is authenticated
-        axios.defaults.headers = setHeader(true);
-      }, []);
-
-    return (
-        <>
-         {/* <Navigate to="/login" replace={true} /> */}
-         <div className="relative">
-            <NavBar />
-            <div className="flex w-full mt-16 z-0">
-                <div className="w-72">
-                <Sidebar />
-                </div>
-                
-                <div className=" w-full">
-                    <Outlet  />
-                    
-                </div>
-
-            </div>
-        </div>
-        </>
         
-    );
+        const user = getAuthUser(); // This function should return the authentication user if it exists
+
+        if (user) {
+            setUser(user);
+            //set default headers if user is authenticated
+            axios.defaults.headers = setHeader(true);
+          } else {
+            setUser(null);
+            navigate("/login", { replace: true });
+           }
+
+      }, [navigate]);
+    
+    if(!user) {
+        return null;
+    }
+    return (
+        <div className="relative">
+          <NavBar />
+          <div className="flex w-full mt-16 z-0">
+            <div className="w-72">
+              <Sidebar />
+            </div>
+            <div className=" w-full">
+              <Outlet />
+            </div>
+          </div>
+        </div>
+      );
+  
 };
 
 export default Index;
+
+
