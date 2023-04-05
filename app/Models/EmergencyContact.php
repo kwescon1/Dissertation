04d4ba21064873366093850adcd887d221ba3373
Odame <kwesi.danquah@daaseng.com>
2023-04-05 13:37:16 +0100

    Merge [integration branch] into [name api_dev_branch]: Pulled [loginResource,client.php,clientfacilitybranch.php,emergencycontact,residence] from [integration branch]

--- app/Models/EmergencyContact.php
+++ app/Models/EmergencyContact.php
@@ -2,17 +2,18 @@
 
 namespace App\Models;
 
-use App\Utils\BaseModel;
 use App\Utils\GeneratesUiud;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class EmergencyContact extends BaseModel
+class EmergencyContact extends Model
 {
     use HasFactory, SoftDeletes, GeneratesUiud;
 
     protected $table = 'emergency_contacts';
+    protected $keyType = "string";
+    protected $guarded = ['id'];
 
     public function client()
     {
