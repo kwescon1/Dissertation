5aa1cb7692715df12a8843db9865dad9b0c3e349
Robert_Rolky <robert.yeboah@daaseng.com>
2023-04-06 06:18:54 +0000

    create all user and all roles

--- resources/js/pages/users&roles/roles/allRoles.jsx
+++ resources/js/pages/users&roles/roles/allRoles.jsx
@@ -1,8 +1,44 @@
+import Table from "../../../components/tables/table";
+import PageTitle from "../../../components/typography/pagetitle";
+import PageContainer from "../../../layouts/pagecontaner";
+import Roles from '../../../static/roles.json'
+
+
 const AllRoles = () => {
+  const columns = [
+    {
+      name: 'Name',
+      selector: row => row.name,
+      sortable: true,
+    },
+    {
+      name: 'Description',
+      selector: row => row.description,
+      
+    },
+    {
+      name: 'Number of Users',
+      cell: (row) => row.number_of_users,
+      
+    },
+    
+    {
+      name: 'Actions',
+      cell: (row) => <div className="flex flex-wrap space-x-4">
+        <button className=" text-primary font-semibold text-base hover:text-primary-100 hover:underline">View</button>
+        <button className=" text-primary font-semibold text-base hover:text-primary-100 hover:underline">Edit</button>
+        <button className=" text-primary font-semibold text-base hover:text-primary-100 hover:underline">Delete</button>
+      </div>,
+    
+    },
+  ];
   return ( 
-    <>
-    Roles
-    </>
+    <div>
+      <PageContainer>
+        <PageTitle title="Roles"/>
+        <Table columns={columns} data={Roles} />
+      </PageContainer>
+    </div>
    );
 }
  
--- resources/js/pages/users&roles/users/allUsers.jsx
+++ resources/js/pages/users&roles/users/allUsers.jsx
@@ -1,7 +1,44 @@
+
+import Table from "../../../components/tables/table";
+import PageTitle from "../../../components/typography/pagetitle";
+import PageContainer from "../../../layouts/pagecontaner";
+import Users from '../../../static/users.json'
+
 const AllUsers = () => {
+  const columns = [
+    {
+      name: 'Name',
+      selector: row => `${row.firstname} ${row.lastname}`,
+      sortable: true,
+    },
+    {
+      name: 'Username',
+      selector: row => row.username,
+      
+    },
+    {
+      name: 'Status',
+      cell: (row) => row.status,
+      
+    },
+    
+    {
+      name: 'Actions',
+      cell: (row) => <div className="flex flex-wrap space-x-4">
+        <button className=" text-primary font-semibold text-base hover:text-primary-100 hover:underline">View</button>
+        <button className=" text-primary font-semibold text-base hover:text-primary-100 hover:underline">Edit</button>
+        <button className=" text-primary font-semibold text-base hover:text-primary-100 hover:underline">Delete</button>
+      </div>,
+    
+    },
+  ];
+  
   return (  
-    <div className="">
-      users
+    <div>
+      <PageContainer>
+        <PageTitle title="Users"/>
+        <Table columns={columns} data={Users} />
+      </PageContainer>
     </div>
     
     
