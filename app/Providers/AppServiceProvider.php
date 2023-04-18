da753e812ca3737b9c9b5905316b05ceb1eeab71
Odame <kwesi.danquah@daaseng.com>
2023-04-18 01:49:42 +0100

    Pull AppServiceProvider.php from integration branch

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Carbon\Carbon;
 use App\Services\Api\Auth\AuthService;
-use App\Services\Api\Role\RoleService;
 use App\Services\Api\User\UserService;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
@@ -12,7 +11,6 @@ use Illuminate\Support\Facades\Response;
 use App\Services\Api\Client\ClientService;
 use App\Services\Api\Facility\FacilityService;
 use App\Services\Api\Auth\AuthServiceInterface;
-use App\Services\Api\Role\RoleServiceInterface;
 use App\Services\Api\User\UserServiceInterface;
 use Illuminate\Http\Resources\Json\JsonResource;
 use App\Services\Api\Client\ClientServiceInterface;
@@ -21,6 +19,8 @@ use App\Services\Api\FacilityBranch\FacilityBranchService;
 use App\Services\Api\UserFacilityBranch\UserFacilityBranchService;
 use App\Services\Api\FacilityBranch\FacilityBranchServiceInterface;
 use App\Services\Api\ClientFacilityBranch\ClientFacilityBranchService;
+use App\Services\Api\Role\RoleService;
+use App\Services\Api\Role\RoleServiceInterface;
 use App\Services\Api\UserFacilityBranch\UserFacilityBranchServiceInterface;
 use App\Services\Api\ClientFacilityBranch\ClientFacilityBranchServiceInterface;
 
@@ -39,9 +39,9 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(FacilityServiceInterface::class, FacilityService::class);
         $this->app->bind(FacilityBranchServiceInterface::class, FacilityBranchService::class);
         $this->app->bind(UserFacilityBranchServiceInterface::class, UserFacilityBranchService::class);
-        $this->app->bind(RoleServiceInterface::class, RoleService::class);
         $this->app->bind(ClientServiceInterface::class, ClientService::class);
         $this->app->bind(ClientFacilityBranchServiceInterface::class, ClientFacilityBranchService::class);
+        $this->app->bind(RoleServiceInterface::class, RoleService::class);
 
 
         $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
@@ -84,6 +84,7 @@ class AppServiceProvider extends ServiceProvider
         Response::macro('error', function ($error, $statusCode = \Illuminate\Http\Response::HTTP_INTERNAL_SERVER_ERROR) {
             return response()->json([
                 'error' => $error,
+                'status' => $statusCode,
 
             ], $statusCode);
         });
