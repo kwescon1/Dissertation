1044273c5b2a255df7848b7d27fd6b47238625aa
Odame <kwesi.danquah@daaseng.com>
2023-04-19 10:43:00 +0100

    delete unwanted file

--- app/Services/Api/Auth/AuthServiceInterface.php
+++ app/Services/Api/Auth/AuthServiceInterface.php
@@ -2,10 +2,13 @@
 
 namespace App\Services\Api\Auth;
 
+use Illuminate\Http\Request;
+
 interface AuthServiceInterface
 {
 
     const LOGIN_CACHE_SECONDS = 60 * 60 * 24;
 
     public function loginUser($data): ?object;
+    public function logout(Request $request): bool;
 }
