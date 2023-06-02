0484d3edb0e28d9efbd05b76b447738132916542
Odame <kwesi.danquah@daaseng.com>
2023-06-02 15:53:08 +0100

    OpenAI custom exception

--- app/Exceptions/OpenAiException.php
+++ app/Exceptions/OpenAiException.php
@@ -4,19 +4,20 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Log;
 
 class OpenAiException extends Exception
 {
     //
 
-    public function __construct($message)
+    public function __construct($message, $code = Response::HTTP_FORBIDDEN)
     {
-        parent::__construct($message);
+        parent::__construct($message, $code);
     }
 
     public function render(Request $e)
     {
-        Log::error($this->message);
+        return response()->error($this->message, $this->code);
     }
 }
