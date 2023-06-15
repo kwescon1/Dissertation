43558373dbff9908ca8b22eea8aad750f1c3be37
Odame <kwesi.danquah@daaseng.com>
2023-06-15 11:13:03 +0100

    Dispatch appointment JOB

--- app/Services/Api/Appointment/AppointmentService.php
+++ app/Services/Api/Appointment/AppointmentService.php
@@ -2,9 +2,13 @@
 
 namespace App\Services\Api\Appointment;
 
+use Carbon\Carbon;
 use App\Models\Appointment;
 use App\Services\Api\CoreService;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Gate;
+use App\Jobs\ChangeAppointmentStatus;
+use App\Models\Enums\AppointmentStatus;
 use Illuminate\Database\Eloquent\Collection;
 
 class AppointmentService extends CoreService implements AppointmentServiceInterface
@@ -20,5 +24,22 @@ class AppointmentService extends CoreService implements AppointmentServiceInterf
         }
 
         return Appointment::branch($facilityBranchId)->scheduledDate($date)->with('client:id,firstname,lastname,othernames')->get();
+    }
+
+
+    /**
+     * @return void
+     */
+    public function changeAppointmentStatusToNoShow(): void
+    {
+
+        $date = Carbon::today();
+
+        Appointment::where(function ($query) use ($date) {
+            $query->whereDate('scheduled_at', $date)
+                ->orWhereDate('scheduled_at', '<', $date);
+        })->whereStatus(AppointmentStatus::SCHEDULED)->each(function ($appointment) {
+            dispatch(new ChangeAppointmentStatus($appointment, AppointmentStatus::NO_SHOW));
+        });
     }
 }
